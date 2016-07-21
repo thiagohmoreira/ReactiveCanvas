1a89e86294323b42be5f0aa67ecb820d208fb663
Thiago Hummel Moreira <contato@thiagomoreira.info>
2016-07-21 01:46:06 -0300

    Implementing visual feedback on rules details.

--- src/js/component/canvas-control.js
+++ src/js/component/canvas-control.js
@@ -3,17 +3,22 @@ import React from 'react';
 import CircleList from '../component/circle-list';
 import { MAX_CIRCLES, getCirclesLeft, getMaxNewRadius } from '../api/canvas';
 
-export default ({ circles, viewport, actions }) => (
-  <div className='canvas-control'>
-    <h3>Canvas Control</h3>
-    <p>Rules:</p>
-    <ul>
-      <li>You can add up to {MAX_CIRCLES} circles ({getCirclesLeft(circles)} left)</li>
-      <li>
-          The sum of diameters cannot be larger than
-          the viewport width ({getMaxNewRadius(circles, viewport.width)} radius left)
-      </li>
-    </ul>
-    <CircleList circles={circles} viewport={viewport} actions={actions} />
-  </div>
-);
+export default ({ circles, viewport, actions }) => {
+  const circlesLeft = getCirclesLeft(circles);
+  const radiusLeft = getMaxNewRadius(circles, viewport.width);
+
+  return (
+    <div className='canvas-control'>
+      <h3>Canvas Control</h3>
+      <p>Rules:</p>
+      <ul>
+        <li>You can add up to {MAX_CIRCLES} circles (<span className={circlesLeft ? null : 'red' }>{circlesLeft} left</span>)</li>
+        <li>
+            The sum of diameters cannot be larger than
+            the viewport width (<span className={radiusLeft ? null : 'red' }>{radiusLeft} radius left</span>)
+        </li>
+      </ul>
+      <CircleList circles={circles} viewport={viewport} actions={actions} />
+    </div>
+  );
+}
